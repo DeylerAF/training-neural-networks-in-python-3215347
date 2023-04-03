53d202b04bd7b809cbc7f072e3b575a678bcd2a9
Deyler Alvarez Fernández <70861729+DeylerAF@users.noreply.github.com>
2023-04-03 17:49:13 +0000

    Solution

--- src/MLP.py
+++ src/MLP.py
@@ -2,13 +2,13 @@ import numpy as np
 
 class Perceptron:
     """A single neuron with the sigmoid activation function.
-       Attributes:
-          inputs: The number of inputs in the perceptron, not counting the bias.
-          bias:   The bias term. By defaul it's 1.0."""
+        Attributes:
+            inputs: The number of inputs in the perceptron, not counting the bias.
+            bias:   The bias term. By default it's 1.0."""
 
     def __init__(self, inputs, bias = 1.0):
         """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
-        self.weights = (np.random.rand(inputs+1) * 2) - 1 
+        self.weights = (np.random.rand(inputs+1) * 2) - 1
         self.bias = bias
 
     def run(self, x):
